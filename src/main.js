7eb34c31cff67f26aa60e2d73ae5482b15c28936
981545521@qq.com <f52007825>
2019-12-06 15:27:31 +0800

    导入所有组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+/**
+ * Vant UI
+ * 参考网址： https://youzan.github.io/vant/?source=vuejsorg#/zh-CN/quickstart
+ */
+import Vant from 'vant';
+import 'vant/lib/index.css'
+Vue.use(Vant);
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
